42ec792ab5817cd5ec55a2540d4d01ac381e563b
Votre Nom <h.hassen.malek@gmail.com>
2024-09-18 12:12:16 +0200

    creation des dossiers pour le back

--- les-mobylettes/src/main/java/com/lesbonnesoeufs/lesmobylettes/lesmobylettes/Controllers/ProductController.java
+++ les-mobylettes/src/main/java/com/lesbonnesoeufs/lesmobylettes/lesmobylettes/Controllers/ProductController.java
@@ -1,6 +1,5 @@
 package com.lesbonnesoeufs.lesmobylettes.lesmobylettes.Controllers;
 
-
 import com.lesbonnesoeufs.lesmobylettes.lesmobylettes.Exceptions.ResourcesNotFoundException;
 import com.lesbonnesoeufs.lesmobylettes.lesmobylettes.Models.Product;
 import com.lesbonnesoeufs.lesmobylettes.lesmobylettes.Repository.ProductRepo;
@@ -15,48 +14,49 @@ import java.util.List;
 public class ProductController {
     private final ProductRepo productRepo;
 
-
     public ProductController(ProductRepo productRepo) {
         this.productRepo = productRepo;
     }
 
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
+        System.out.println("coucou");
         return productRepo.findAll();
     }
 
     @GetMapping("/products/{id}")
-    public Product getProductByID(@PathVariable(value = "id") Long ProdID) throws ResourcesNotFoundException {
-        return productRepo.findById(ProdID)
-                .orElseThrow(() ->new ResourcesNotFoundException("Spot avec ID : "+ProdID +" est inexistant"));
+    public Product getProductByID(@PathVariable(value = "id") Long prodID) throws ResourcesNotFoundException {
+        return productRepo.findById(prodID)
+                .orElseThrow(() -> new ResourcesNotFoundException("Spot avec ID : " + prodID + " est inexistant"));
     }
-    @DeleteMapping("/products{id}")
-        public void deleteProduct(@PathVariable(value = "id")Long ProdID) throws ResourcesNotFoundException{
-            Product prod = productRepo.findById(ProdID)
-                 .orElseThrow(() ->new ResourcesNotFoundException("Spot avec ID : "+ProdID +" est inexistant"));
 
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable(value = "id") Long prodID) throws ResourcesNotFoundException {
+        Product prod = productRepo.findById(prodID)
+                .orElseThrow(() -> new ResourcesNotFoundException("Spot avec ID : " + prodID + " est inexistant"));
         productRepo.delete(prod);
     }
-    @PutMapping("/products{id}")
-    public Product updateProduct(@PathVariable(value = "id") Long ProdID,
-                                 @Validated @RequestBody Product proDetails)throws ResourcesNotFoundException{
-        Product prod = productRepo.findById(ProdID)
-                .orElseThrow(()->new ResourcesNotFoundException("Spot avec ID : "+ProdID +" est inexistant"));
-        prod.setSurfBreak(proDetails.getSurfBreak());
-        prod.setDifficultyLevel(proDetails.getDifficultyLevel());
-        prod.setDestination(proDetails.getDestination() );
-        prod.setGeocode(proDetails.getGeocode());
-        prod.setPeakSurfSeasonBegins(proDetails.getPeakSurfSeasonBegins());
-        prod.setPeakSurfSeasonEnds(proDetails.getPeakSurfSeasonEnds());
-        prod.setDestinationStateCountry(proDetails.getDestinationStateCountry());
-        prod.setAdress(proDetails.getAdress());
 
+    @PutMapping("/products/{id}")
+    public Product updateProduct(@PathVariable(value = "id") Long prodID,
+                                 @Validated @RequestBody Product prodDetails) throws ResourcesNotFoundException {
+        Product prod = productRepo.findById(prodID)
+                .orElseThrow(() -> new ResourcesNotFoundException("Spot avec ID : " + prodID + " est inexistant"));
+        prod.setSurfBreak(prodDetails.getSurfBreak());
+        prod.setDifficultyLevel(prodDetails.getDifficultyLevel());
+        prod.setDestination(prodDetails.getDestination());
+        prod.setGeocode(prodDetails.getGeocode());
+        prod.setPeakSurfSeasonBegins(prodDetails.getPeakSurfSeasonBegins());
+        prod.setPeakSurfSeasonEnds(prodDetails.getPeakSurfSeasonEnds());
+        prod.setDestinationStateCountry(prodDetails.getDestinationStateCountry());
+        prod.setAddress(prodDetails.getAddress());
+        prod.setPhotoUrl(prodDetails.getPhotoUrl());
 
         return productRepo.save(prod);
     }
+
     @PostMapping("/products")
-    public Product creatProd(@Validated @RequestBody Product prod){
+    public Product createProduct(@Validated @RequestBody Product prod) {
         return productRepo.save(prod);
     }
-
 }
--- les-mobylettes/src/main/java/com/lesbonnesoeufs/lesmobylettes/lesmobylettes/Models/Product.java
+++ les-mobylettes/src/main/java/com/lesbonnesoeufs/lesmobylettes/lesmobylettes/Models/Product.java
@@ -1,20 +1,16 @@
 package com.lesbonnesoeufs.lesmobylettes.lesmobylettes.Models;
 
-
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
-import java.util.List;
-import java.util.ArrayList;
 
 @Data
 @Entity
-@Table(name = "t_Product")
+@Table(name = "t_product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
 
     @Column(length = 255)
@@ -32,12 +28,11 @@ public class Product {
     private Date peakSurfSeasonEnds;
 
     @Column(length = 255)
-    private String DestinationStateCountry;
+    private String destinationStateCountry;
 
     @Column(length = 255)
-    private String adress;
+    private String address;  // Corrigé de 'adress' à 'address'
 
-    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Photo> photos = new ArrayList<>();
+    @Column(length = 2048)
+    private String photoUrl;
 }
-
